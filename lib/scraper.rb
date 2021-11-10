f9b0f5725882f32fa7f59d620507bc538af63eaa
Wilfred <wilfredbayudan@gmail.com>
2021-11-09 15:01:39 -1000

    commit tree 19e8481100684790ec92877626905b4dfdc45c7e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(URI.open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN").text
+
+puts doc.css(".headline-26OIBN")
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
